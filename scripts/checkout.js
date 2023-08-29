27287a144317a9a8a2d3a52eb48b46428156d955
ikhwanizao <ikhwanm32@gmail.com>
2023-08-29 10:38:04 +0800

    update checkout.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -16,7 +16,9 @@ cart.forEach((cartItem) => {
   });
 
   cartSummaryHTML += `
-    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+    <div class="cart-item-container js-cart-item-container-${
+      matchingProduct.id
+    }">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -35,13 +37,15 @@ cart.forEach((cartItem) => {
                 <div class="product-quantity">
                     <span>
                     Quantity: <span class="quantity-label">${
-                    cartItem.quantity
+                      cartItem.quantity
                     }</span>
                     </span>
                     <span class="update-quantity-link link-primary">
                     Update
                     </span>
-                    <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+                    <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${
+                      matchingProduct.id
+                    }">
                     Delete
                     </span>
                 </div>
@@ -98,11 +102,13 @@ cart.forEach((cartItem) => {
 
 document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
 
-document.querySelectorAll('.js-delete-link').forEach(link => {
-    link.addEventListener('click', () => {
-        const productId = link.dataset.productId;
-        removeFromCart(productId);
-        const container = document.querySelector(`.js-cart-item-container-${productId}`);
-        container.remove();
-    });
+document.querySelectorAll(".js-delete-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    const productId = link.dataset.productId;
+    removeFromCart(productId);
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.remove();
+  });
 });
