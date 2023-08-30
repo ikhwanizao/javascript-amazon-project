78709ed6e8b39c4a688113f15fe9b1c63b87bf7f
ikhwanizao <ikhwanm32@gmail.com>
2023-08-30 10:41:04 +0800

    Quantity and Update link disappear

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -41,8 +41,8 @@ cart.forEach((cartItem) => {
                     }</span>
                     </span>
                     <span class="update-quantity-link link-primary js-update-link" data-product-id="${
-						matchingProduct.id
-					  }">
+                      matchingProduct.id
+                    }">
                     Update
                     </span>
 					<input class="quantity-input"><span class="save-quantity-link link-primary">Save</span>
@@ -118,10 +118,13 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
   });
 });
 
-
 document.querySelectorAll(".js-update-link").forEach((link) => {
   link.addEventListener("click", () => {
     const productId = link.dataset.productId;
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.classList.add("is-editing-quantity");
   });
 });
 
